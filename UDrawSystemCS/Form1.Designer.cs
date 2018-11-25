95ab3e762ff72ef18ca55adaffa9057fd0939fa0
seafield1979 <seafield1979@gmail.com>
2018-11-25 17:17:27 +0900

    各描画を切り替えるスイッチ処理を追加

--- UDrawSystemCS/Form1.Designer.cs
+++ UDrawSystemCS/Form1.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Test1");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Test2");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Test3");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Draw", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3});
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Test1");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Test2");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Test3");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Button", new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode6,
+            treeNode7});
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Test1");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Test2");
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Test3");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("List", new System.Windows.Forms.TreeNode[] {
+            treeNode9,
+            treeNode10,
+            treeNode11});
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Test1");
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("Test2");
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Test3");
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("ScrollBar", new System.Windows.Forms.TreeNode[] {
+            treeNode13,
+            treeNode14,
+            treeNode15});
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.treeView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 567);
+            this.panel1.TabIndex = 0;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(200, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 567);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "DrawTest1";
+            treeNode1.Tag = "101";
+            treeNode1.Text = "Test1";
+            treeNode2.Name = "DrawTest2";
+            treeNode2.Tag = "102";
+            treeNode2.Text = "Test2";
+            treeNode3.Name = "DrawTest3";
+            treeNode3.Tag = "103";
+            treeNode3.Text = "Test3";
+            treeNode4.Name = "DrawNode";
+            treeNode4.Text = "Draw";
+            treeNode5.Name = "ButtonTest1";
+            treeNode5.Tag = "201";
+            treeNode5.Text = "Test1";
+            treeNode6.Name = "ButtonTest2";
+            treeNode6.Tag = "202";
+            treeNode6.Text = "Test2";
+            treeNode7.Name = "ButtonTest3";
+            treeNode7.Tag = "203";
+            treeNode7.Text = "Test3";
+            treeNode8.Name = "ButtonNode";
+            treeNode8.Text = "Button";
+            treeNode9.Name = "ListTest1";
+            treeNode9.Tag = "301";
+            treeNode9.Text = "Test1";
+            treeNode10.Name = "ListTest2";
+            treeNode10.Tag = "302";
+            treeNode10.Text = "Test2";
+            treeNode11.Name = "ListTest3";
+            treeNode11.Tag = "303";
+            treeNode11.Text = "Test3";
+            treeNode12.Name = "ListNode";
+            treeNode12.Text = "List";
+            treeNode13.Name = "SBTest1";
+            treeNode13.Tag = "401";
+            treeNode13.Text = "Test1";
+            treeNode14.Name = "SBTest2";
+            treeNode14.Tag = "402";
+            treeNode14.Text = "Test2";
+            treeNode15.Name = "SBTest3";
+            treeNode15.Tag = "403";
+            treeNode15.Text = "Test3";
+            treeNode16.Name = "ScrollBarNode";
+            treeNode16.Text = "ScrollBar";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode8,
+            treeNode12,
+            treeNode16});
+            this.treeView1.Size = new System.Drawing.Size(200, 567);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(203, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(610, 567);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 567);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- UDrawSystemCS/Form1.cs
+++ UDrawSystemCS/Form1.cs
@@ -1,20 +1,150 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace UDrawSystemCS
 {
+    public enum EDrawMode : int
+    {
+        Draw1 = 0,
+        Draw2,
+        Draw3,
+        Button1,
+        Button2,
+        Button3,
+        List1,
+        List2,
+        List3,
+        SB1,
+        SB2,
+        SB3
+    }
+
     public partial class Form1 : Form
     {
+        //
+        // Properties
+        //
+        #region プロパティ
+
+        public EDrawMode drawMode;
+
+        #endregion プロパティ
+
         public Form1()
         {
             InitializeComponent();
+
+            Initialize();
         }
+
+        // TreeViewの項目が選択された
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            Console.WriteLine(e.Node.Text);
+
+            switch (e.Node.Tag)
+            {
+                case "101":
+                    drawMode = EDrawMode.Draw1;
+                    break;
+                case "102":
+                    drawMode = EDrawMode.Draw2;
+                    break;
+                case "103":
+                    drawMode = EDrawMode.Draw3;
+                    break;
+                case "201":
+                    drawMode = EDrawMode.Button1;
+                    break;
+                case "202":
+                    drawMode = EDrawMode.Button2;
+                    break;
+                case "203":
+                    drawMode = EDrawMode.Button3;
+                    break;
+                case "301":
+                    drawMode = EDrawMode.List1;
+                    break;
+                case "302":
+                    drawMode = EDrawMode.List2;
+                    break;
+                case "303":
+                    drawMode = EDrawMode.List3;
+                    break;
+                case "401":
+                    drawMode = EDrawMode.SB1;
+                    break;
+                case "402":
+                    drawMode = EDrawMode.SB2;
+                    break;
+                case "403":
+                    drawMode = EDrawMode.SB3;
+                    break;
+                default:
+                    return;
+            }
+            panel2.Invalidate();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            switch (drawMode)
+            {
+                case EDrawMode.Draw1:
+                    DrawDraw1(g);
+                    break;
+                case EDrawMode.Draw2:
+                    DrawDraw2(g);
+                    break;
+                case EDrawMode.Draw3:
+                    DrawDraw3(g);
+                    break;
+                case EDrawMode.Button1:
+                    break;
+                case EDrawMode.Button2:
+                    break;
+                case EDrawMode.Button3:
+                    break;
+                case EDrawMode.List1:
+                    break;
+                case EDrawMode.List2:
+                    break;
+                case EDrawMode.List3:
+                    break;
+                case EDrawMode.SB1:
+                    break;
+                case EDrawMode.SB2:
+                    break;
+                case EDrawMode.SB3:
+                    break;
+            }
+        }
+
+        #region メソッド
+        private void Initialize()
+        {
+            drawMode = EDrawMode.Draw1;
+        }
+        #endregion メソッド
+
+        #region DrawModeメソッド
+
+        private void DrawDraw1(Graphics g)
+        {
+            g.FillRectangle(Brushes.Red, 100, 100, 100, 100);
+        }
+        private void DrawDraw2(Graphics g)
+        {
+            g.FillRectangle(Brushes.Green, 100, 100, 100, 100);
+        }
+        private void DrawDraw3(Graphics g)
+        {
+            g.FillRectangle(Brushes.Blue, 100, 100, 100, 100);
+        }
+
+        #endregion DrawModeメソッド
     }
 }
